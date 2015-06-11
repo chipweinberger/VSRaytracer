ca28041458ba7d1010ae4c7c4d189c80b39c740c
Chip Weinberger <weinberger.c@gmail.com>
2015-06-10 19:40:20 -0500

    init

---
+++ VSRayTracer/raytrace.js
@@ -0,0 +1,85 @@
+//# sourceMappingURL=app.js.map
+/// <reference path="index.html"/>
+
+
+
+
+var canvas = document.getElementById("raytrace")
+
+
+function initRaytrace() {
+
+
+}
+
+
+var antialiasing = {
+
+    jittered: true,
+    n: 4,
+
+}
+
+function raytrace() {
+
+    var width = canvas.getAttribute("width")
+    var height = canvas.getAttribute("height")
+
+    //dir to upper left
+    var RotYbig = new THREE.Matrix4().makeRotationY(THREE.Math.degToRad(MAIN_fov) / 2)
+    var RotXbig = new THREE.Matrix4().makeRotationX(THREE.Math.degToRad(MAIN_fov) / 2)
+    var upper_left = MAIN_at.clone().applyMatrix4(RotYbig).applyMatrix4(RotXbig)
+
+    //shoot rays
+    for (x = 0; x < width; x++) {
+        for (y = 0; y < height; y++) {
+
+            var avg_pix = new THREE.Vector3(0,0,0)
+
+            //antialiasing
+            for (subx = x ; subx < x + 1 ; subx+= 1/ antialiasing.n ) {
+                for (suby = y ; suby < y + 1 ; suby += 1 / antialiasing.n) {
+
+                    var x2 = subx //clone
+                    var y2 = suby //clone
+
+                    //org
+                    var org = new THREE.Vector3(0, 0, 0)
+
+                    //jitter
+                    if (antialising.jittered) {
+                        var step = 1 / antialising.n
+                        x2 = THREE.Math.randFloat(x2, x2 + step)
+                        y2 = THREE.Math.randFloat(y2, y2 + step)
+                    }
+
+                    var RotY = new THREE.Matrix4().makeRotationY(THREE.Math.degToRad(x2))
+                    var RotX = new THREE.Matrix4().makeRotationX(THREE.Math.degToRad(y2))
+                    var dir = upper_left.clone().applyMatrix4(RotY).applyMatrix4(RotX)
+
+                    //apply view matrix
+                    org.applyMatrix4(MAIN_viewMatrix)
+                    dir.applyMatrix4(MAIN_viewMatrix)
+
+                    var color = trace(org, dir)
+                    avg_pix += color / (antialiasing.n * antialiasing.n)
+
+                }
+            }
+
+            //set pixel color
+            var ctx = c.getContext("2d");
+            var p = avg_pix
+            ctx.fillStyle = "rgba(" + p.x + "," + p.y + "," + p.z + "," + 1 + ")";
+            ctx.fillRect(x, y, 1, 1);
+
+        }
+    }
+}
+
+//traces a single ray
+function trace(org, dir) {
+
+    return new THREE.Vector3(Math.random(), Math.random(),Math.random())
+
+}
---
+++ VSRayTracer/webgl.js
@@ -0,0 +1,55 @@
+﻿
+
+var canvas = document.getElementById("webgl")
+
+var SCREEN_WIDTH = canvas.getAttribute("width")
+var SCREEN_HEIGHT = canvas.getAttribute("height")
+
+var camera, scene, renderer, mesh;
+var cameraPerspective;
+var renderer;
+
+
+function initWebgl() {
+
+    // renderer
+    renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // scene
+    scene = new THREE.Scene()
+
+
+    // camera
+    camera = new THREE.Camera();
+    scene.add(camera)
+
+    camera.projectionMatrix = MAIN_viewMatrix
+
+
+    var sphere = new THREE.Mesh(new THREE.SphereGeometry(2, 100, 100), new THREE.MeshNormalMaterial());
+    sphere.position.set(0, 1, -8)
+    sphere.overdraw = true;
+    scene.add(sphere);
+
+
+    var sphere = new THREE.Mesh(new THREE.SphereGeometry(2, 100, 100), new THREE.MeshNormalMaterial());
+    sphere.position.set(3, 0, -2)
+    sphere.overdraw = true;
+    scene.add(sphere);
+
+    var plane = new THREE.Mesh(new THREE.PlaneGeometry(30, 20), new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide }));
+    plane.lookAt(new THREE.Vector3(0, 100, 0))
+    scene.add(plane);
+
+
+}
+
+function renderWebgl() {
+
+    camera.projectionMatrix = MAIN_viewMatrix
+
+    renderer.render(scene, camera);
+
+}
+
