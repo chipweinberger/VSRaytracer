fa45c6f71bcd631e7aad393d11350db05fc9cf91
Chip Weinberger <weinberger.c@gmail.com>
2015-06-10 21:24:13 -0500

    2

---
+++ VSRayTracer/main.js
@@ -0,0 +1,81 @@
+/// <reference path="webgl.ts"/>
+/// <reference path="raytrace.ts"/>
+//default parameters shared by raytracer and webgl
+var MAIN_pos = new THREE.Vector3(0, 4, 20);
+var MAIN_at = new THREE.Vector3(0, 3, 200).normalize();
+var MAIN_up = new THREE.Vector3(0, -200, 0).normalize();
+var MAIN_fov = 70;
+var MAIN_transMatrix = new THREE.Matrix4().makeTranslation(MAIN_pos.x, MAIN_pos.y, MAIN_pos.z);
+var MAIN_rotMatrix = new THREE.Matrix4().lookAt(new THREE.Vector3(0, 0, 0), MAIN_at, MAIN_up);
+var MAIN_perspective = new THREE.Matrix4().makePerspective(MAIN_fov, 1, 0.1, 1000);
+var MAIN_viewMatrix = MAIN_perspective.clone().multiply(MAIN_rotMatrix).multiply(MAIN_transMatrix);
+var object_list = [
+    { type: "sphere", pos: new THREE.Vector3(1, -1, -8), size: 2 },
+    { type: "sphere", pos: new THREE.Vector3(0, -3, -6), size: 3 },
+    { type: "plane", pos: new THREE.Vector3(0, 0, 0), at: new THREE.Vector3(0, 1, 0) }
+];
+window.addEventListener("keydown", function (ev) {
+    var rotSpeed = 0.04;
+    var transSpeed = .1;
+    var transX = new THREE.Matrix4().makeTranslation(transSpeed, 0, 0);
+    var transY = new THREE.Matrix4().makeTranslation(0, transSpeed, 0);
+    var transZ = new THREE.Matrix4().makeTranslation(0, 0, transSpeed);
+    switch (ev.keyCode) {
+        case 84:
+            raytrace();
+            break;
+        case 39:
+            MAIN_at.applyAxisAngle(MAIN_up, rotSpeed);
+            break;
+        case 37:
+            MAIN_at.applyAxisAngle(MAIN_up, -rotSpeed);
+            break;
+        case 38:
+            var perpendicular = new THREE.Vector3().crossVectors(MAIN_at, MAIN_up).normalize();
+            MAIN_at.applyAxisAngle(perpendicular, rotSpeed);
+            MAIN_up.applyAxisAngle(perpendicular, rotSpeed);
+            break;
+        case 40:
+            var perpendicular = new THREE.Vector3().crossVectors(MAIN_at, MAIN_up).normalize();
+            MAIN_at.applyAxisAngle(perpendicular, -rotSpeed);
+            MAIN_up.applyAxisAngle(perpendicular, -rotSpeed);
+            break;
+        case 65:
+            MAIN_transMatrix.multiply(transX);
+            break;
+        case 90:
+            MAIN_transMatrix.multiply(new THREE.Matrix4().getInverse(transX));
+            break;
+        case 83:
+            MAIN_transMatrix.multiply(transY);
+            break;
+        case 88:
+            MAIN_transMatrix.multiply(new THREE.Matrix4().getInverse(transY));
+            break;
+        case 68:
+            MAIN_transMatrix.multiply(transZ);
+            break;
+        case 67:
+            MAIN_transMatrix.multiply(new THREE.Matrix4().getInverse(transZ));
+            break;
+        case 70:
+            MAIN_up.applyAxisAngle(MAIN_at, rotSpeed);
+            break;
+        case 86:
+            MAIN_up.applyAxisAngle(MAIN_at, -rotSpeed);
+            break;
+    }
+    //set view matrix
+    MAIN_rotMatrix = new THREE.Matrix4().lookAt(new THREE.Vector3(0, 0, 0), MAIN_at, MAIN_up);
+    MAIN_viewMatrix = MAIN_perspective.clone().multiply(MAIN_rotMatrix).multiply(MAIN_transMatrix).multiply(MAIN_transMatrix);
+});
+function draw() {
+    renderWebgl();
+    window.requestAnimationFrame(draw);
+}
+function main() {
+    initWebgl();
+    initRaytrace();
+    window.requestAnimationFrame(draw);
+}
+//# sourceMappingURL=main.js.map
--- VSRayTracer/webgl.ts
+++ VSRayTracer/webgl.ts
@@ -1,38 +1,59 @@
-var canvas = document.getElementById("webgl");
-var SCREEN_WIDTH = canvas.getAttribute("width");
-var SCREEN_HEIGHT = canvas.getAttribute("height");
+﻿
+
+
+
+var canvas = document.getElementById("webgl")
+
+var SCREEN_WIDTH = canvas.getAttribute("width")
+var SCREEN_HEIGHT = canvas.getAttribute("height")
+
 var camera, scene, renderer, mesh;
 var cameraPerspective;
 var renderer;
+
+
 function initWebgl() {
+
     // renderer
-    renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    renderer = new THREE.WebGLRenderer({ canvas: <any> canvas });
     renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
     // scene
-    scene = new THREE.Scene();
+    scene = new THREE.Scene()
+
+
     // camera
     camera = new THREE.Camera();
-    scene.add(camera);
-    camera.projectionMatrix = MAIN_viewMatrix;
+    scene.add(camera)
+
+    camera.projectionMatrix = MAIN_viewMatrix
+
+
     for (var i in object_list) {
-        var obj = object_list[i];
+        var obj = <any> object_list[i]
         switch (obj.type) {
             case "sphere":
                 var sp = new THREE.Mesh(new THREE.SphereGeometry(obj.size, 100, 100), new THREE.MeshNormalMaterial());
-                sp.position.set(obj.pos.x, obj.pos.y, obj.pos.z);
+                sp.position.set(obj.pos.x, obj.pos.y, obj.pos.z) 
                 scene.add(sp);
                 break;
             case "plane":
                 var plane = new THREE.Mesh(new THREE.PlaneGeometry(50, 50), new THREE.MeshBasicMaterial({ color: 0xA0888888, side: THREE.DoubleSide }));
-                plane.position.set(obj.pos.x, obj.pos.y, obj.pos.z);
-                plane.lookAt(obj.at);
+                plane.position.set(obj.pos.x, obj.pos.y, obj.pos.z) 
+                plane.lookAt(obj.at)
                 scene.add(plane);
                 break;
+        
         }
     }
+
 }
+
 function renderWebgl() {
-    camera.projectionMatrix = MAIN_viewMatrix;
+
+    camera.projectionMatrix = MAIN_viewMatrix
+
     renderer.render(scene, camera);
+
 }
-//# sourceMappingURL=webgl.js.map
+
